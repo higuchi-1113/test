5aeabe5d81cf249bb8c377b385c654ea66625c3d
higuchi-1113 <s-higuchi@openupitengineer.jp>
2024-12-09 16:17:57 +0900

    確認テスト振り返り

--- test.py
+++ test.py
@@ -1,6 +1,38 @@
+"""
 student = {"name": "鈴木", "class": "3A"}
 student["score"] = 85
 del student["class"]
 print(student)
 
 print("更新表示")
+
+
+age = input("年齢を入力してください：")
+print("あなたは" + age + "歳です")
+
+def add(x, y):
+
+    return x + y
+
+result = add(1,3)
+
+print(result)
+
+
+def multiply(a, b=2):
+    return a * b
+print(multiply(3))
+"""
+
+def modify_value(x):
+    x = x + 1
+    return x
+
+num = 6
+modify_value(num)
+print(num)
+
+def calculate_points(score, bonus=10):
+    return score + bonus
+print(calculate_points(80))
+print(calculate_points(80, 20))
